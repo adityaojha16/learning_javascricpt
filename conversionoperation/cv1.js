1c7165362962cc32869ea17885cc3b906319e524
ADITYA RAJ <aditya20nov2007@gmail.com>
2026-02-03 11:25:25 +0000

    basic_coversion

---
+++ conversionoperation/cv1.js
@@ -0,0 +1,21 @@
+// let score="97";
+
+// console.log(typeof (score));
+// let valueInNumber= Number(score)
+
+// console.log(typeof valueInNumber);
+
+let newscore="254kjh";
+let isteamwin= 1;
+console.log(typeof newscore);
+
+let CON= Number(newscore);
+console.log(typeof CON);
+console.log(CON);
+let BOW= Boolean(isteamwin);
+console.log(BOW);
+
+
+// "97"=97;
+// "254kjh"=NaN;(not a number)
+// true=1; false =0;
