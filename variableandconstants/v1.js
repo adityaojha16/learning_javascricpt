5d343831cbfe71467c484024e4a5e228695a5b51
ADITYA RAJ <aditya20nov2007@gmail.com>
2026-02-03 07:49:27 +0000

    configure done

---
+++ variableandconstants/v1.js
@@ -0,0 +1,11 @@
+const accountId= 78523;
+let UserName="Aditya"
+let Useremail="aditya20nov2007"
+let accountPasskey="124578"
+let usercity="motihari"
+let Userstate;
+
+
+accountPasskey="986532"
+// console.log(accountId);
+console.table([accountId, UserName, Useremail, accountPasskey, usercity,Userstate])
