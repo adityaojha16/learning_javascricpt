4fc9dc8fcf0a23d1e79b39d14e00a9b9216cdbd1
ADITYA RAJ <aditya20nov2007@gmail.com>
2026-02-04 08:43:20 +0000

    comparision operator

---
+++ comparision/c1.js
@@ -0,0 +1,13 @@
+// console.log("2">1);
+// console.log("02">1);
+// console.log("23"===23);
+
+// console.log(0==undifined);
+
+
+console.log(null >=0);
+console.log(null ==0);
+console.log(null <=0);
+
+// === equals to and type of
+console.log("23"==="9");
