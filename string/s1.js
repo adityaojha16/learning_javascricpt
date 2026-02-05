81007b533491ec1db37488b7fcf8e095f3c03aee
ADITYA RAJ <aditya20nov2007@gmail.com>
2026-02-05 09:12:39 +0000

    string done

---
+++ string/s1.js
@@ -0,0 +1,34 @@
+const name="aditya ojha";
+const repocount="12";
+// old methos
+// console.log(name+" and his repo count is "+repocount);
+
+
+// new method
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+const movname =new String('PRACHANYA');
+
+// console.log(movname[0]);
+// console.log(movname.__proto__);
+// console.log(typeof movname);
+// console.log(movname.toLowerCase());
+// console.log(movname.charAt(5));
+// console.log(movname.indexOf('A'));
+
+// let first=movname.indexOf('A');
+// let second=movname.indexOf('A', first+1);
+// console.log(movname.indexOf('A', second+1));
+
+// const newstring= movname.substring(0,5)
+// console.log(newstring)
+
+// const anotherstring= movname.slice(-8,5)
+// console.log(anotherstring)
+
+
+// trim used to remove unwanted spaces
+// const newstring= movname.trim() 
+// console.log(newstring)
+
+console.log(movname.replace('A','E'))
