b9c0ab1c4c5e35ae0ca2a00de0694adcac666dce
ADITYA RAJ <aditya20nov2007@gmail.com>
2026-02-05 10:23:56 +0000

    configure done

---
+++ numsandmaths/n1.js
@@ -0,0 +1,15 @@
+const score=500;
+console.log(score)
+
+const balance= new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+
+const othernumber=145.258
+console.log(othernumber.toPrecision(5));
+
+const hundreds=100000000;
+console.log(hundreds.toLocaleString())
