a9966e2b53d3b821b822c8b7108b83a25d16165d
ADITYA RAJ <aditya20nov2007@gmail.com>
2026-02-06 11:41:45 +0000

    arrays done

---
+++ arrays/a1.js
@@ -0,0 +1,34 @@
+const myARR=[0,1,2,3,4,5,6]
+const myMovies=["BFG","jannantram mannantram","krish","Batman"]
+
+
+// console.log(myMovies[2])
+
+// myARR.push(2)
+// myARR.pop()
+// myARR.unshift(9)
+// myARR.shift()
+
+
+// console.log(myARR.includes(4))
+// console.log(myARR.indexOf(5))
+
+const newArr=myARR.join()
+
+// console.log(myARR);
+// console.log(newArr);
+// console.log(typeof newArr)
+
+console.log("A",myARR)
+
+const myar1=myARR.slice(1,3);
+
+
+console.log(myar1)
+console.log("B",myARR)
+
+
+const myar2=myARR.splice(1,3)
+console.log(myar2)
+console.log("C",myARR)
+
---
+++ arrays/a2.js
@@ -0,0 +1,28 @@
+let marvel_heros=["thanos","dr.Doom","loki"]
+let dc_heros=["superman","batman","joker"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros)
+
+// let newAr1=marvel_heros.concat(dc_heros)
+// console.log(newAr1)
+
+// const all_new_heros=[...marvel_heros,...dc_heros]
+// console.log(all_new_heros)
+
+// const another_array=[1,2,3,[4,5],6,[7],8,9]
+
+// const real_another_array=another_array.flat(Infinity)
+// console.log(real_another_array)
+
+// console.log(Array.isArray("ojha"))
+// console.log(Array.from("ojha"))
+// console.log(Array.from({name:"aditya ojha"}))//imp
+
+let marks1=95;
+let marks2=97;
+let marks3=87;
+
+
+console.log(Array.of(marks1,marks2,marks3))
